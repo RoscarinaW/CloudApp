6f5b896de2b392fdee7de1affe12b0a3a2815f5a
RoscarinaW <oscarinawrn@gmail.com>
2025-11-11 12:43:37 -0500

    Clases 11/11

---
+++ src/main/java/co/edu/pi/cloudapp/cloudapp/security/JwtAutenticationFilter.java
@@ -0,0 +1,43 @@
+package co.edu.pi.cloudapp.cloudapp.security;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAutenticationFilter  extends OncePerRequestFilter{
+    private JwtUtil jwtUtil;
+
+    public JwtAutenticationFilter(JwtUtil jwtUtil){
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filter) throws ServletException, IOException{
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if(authHeader != null && authHeader.startsWith( "Bearer,", 0)) {
+            String token = authHeader.substring(7);
+            try{
+                var claims = jwtUtil.validateTokenAndGetClaims(token);
+                String sub = claims.getSubject();
+                var auth = new UsernamePasswordAuthenticationToken(sub, null, List.of(new SimpleGrantedAuthority("ROLE_USER")));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } catch(Exception ex){
+                SecurityContextHolder.clearContext();
+            }
+        }
+        filter.doFilter(request, response);
+ }
+
+}
