f4847760f09320d8a9e684dd5380dbc28f83f222
RoscarinaW <oscarinawrn@gmail.com>
2025-11-18 11:08:05 -0500

    se agregra clase SecutityConfig y el compontes en JwrUtil

--- src/main/java/co/edu/pi/cloudapp/cloudapp/config/SecurityConfig.java
+++ src/main/java/co/edu/pi/cloudapp/cloudapp/config/SecurityConfig.java
@@ -1,10 +1,37 @@
 package co.edu.pi.cloudapp.cloudapp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import co.edu.pi.cloudapp.cloudapp.security.JwtAutenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+    private final JwtAutenticationFilter jwtFilter;
+
+    public SecurityConfig(JwtAutenticationFilter jwtFilter){
+        this.jwtFilter = jwtFilter;
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) 
+    throws Exception{
+        http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> 
+                sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/v3/api-docs**","/swagger-ui/**","swagger-ui.html","actuator/**","/api/auth/**").permitAll().anyRequest()
+                .authenticated()
+            ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+            return http.build();
+    }
+    
     
 }
--- src/main/java/co/edu/pi/cloudapp/cloudapp/security/JwtUtil.java
+++ src/main/java/co/edu/pi/cloudapp/cloudapp/security/JwtUtil.java
@@ -5,12 +5,14 @@ import java.util.Date;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
+@Component
 public class JwtUtil {
     private final SecretKey key;
     private final long validityMs;
